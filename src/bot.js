a889ef1146593b745f594862ce8be90a112ca723
Mayank <mayankmchandratre@gmail.com>
2024-12-09 00:00:39 +0530

    bug

--- src/bot.js
+++ src/bot.js
@@ -15,11 +15,11 @@ app.get('/tweet', async (req, res) => {
     const message = await generateTweet();
     await postTweet(message);
     console.log('Tweet posted! ' + message);
-    res.send('Tweet posted!');
+    res.json({message: message, success: true});
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3001}`);
 });
 
 console.log("Bot is running...");
