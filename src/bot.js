17042dc94aef3e911118af787830f22ec226fa4d
Mayank <mayankmchandratre@gmail.com>
2024-12-08 23:38:08 +0530

    express

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,24 @@
-import { TwitterApi } from 'twitter-api-v2';
 import dotenv from 'dotenv';
-import xConfig from './config/x.config.js';
-import postTweet from './services/post.service.js';
-
+import express from 'express';
 dotenv.config();
 
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/tweet', async (req, res) => {
+    const message = generateTweet();
+    await postTweet(message);
+    console.log('Tweet posted! ' + message);
+    res.send('Tweet posted!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
 
 console.log("Bot is running...");
 
