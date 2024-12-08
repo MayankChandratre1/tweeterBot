8107f5c9b52ad42c283ca0d3ea698aa239731acd
Mayank <mayankmchandratre@gmail.com>
2024-12-08 23:13:59 +0530

    vercel.json

--- src/bot.js
+++ src/bot.js
@@ -6,13 +6,4 @@ import postTweet from './services/post.service.js';
 dotenv.config();
 
 
-
-    // (async () => {
-    // try {
-    //     const user = await client.v2.me();
-    //     console.log(`Authenticated as ${user.data.username}`);
-    // } catch (error) {
-    //     console.error('Error during authentication:', error);
-    // }
-    // })();
-postTweet("Hello, Twitter! This is my JS bot 🤖");
+startCron();
